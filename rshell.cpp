f98df697bb41e13b52f122334e207c7e8dc1ceb7
Adam Fan <afan004@well.cs.ucr.edu>
2014-10-20 00:01:04 -0700

    fixed some code

--- rshell.cpp
+++ rshell.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<stdlib.h>
+#include<unistd.h>
 using namespace std;
 
-int main()
+int main(int arc, char *argv[])
 {
+	while(1)
+	{
+		bool clause = true;
+	//	char *token = strtok(a, ";");
+		char token[128] = {};
+		cout << "$";
+		cin.getline(token, 128);
+		if(strcmp(token,"exit") == 0)
+		{
+			exit(0);//if user typed exit, it exits the program.
+		}
+		char *cmptoken = strtoken(token, " ");
+		for (int i = 0; i < 128 ;i++)
+		{
+			arg[i] = cmptoken;
+			
+		}
+			
+	}
+
 	return 0;
 }
