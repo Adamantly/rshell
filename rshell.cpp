b0b37a2ee4846ca70b9d0c96900b3ee1d01a3473
Adam Fan <afan004@hammer.cs.ucr.edu>
2014-10-21 22:37:49 -0700

    cleaned some code

--- rshell.cpp
+++ rshell.cpp
@@ -9,17 +9,25 @@
 #include<sys/wait.h>
 using namespace std;
 
-int main(int argc, char *argv[])
+
+void login()
 {
-	while(1)
-	{
-		char token[1024] = {0};
 		char username[128];
 		char hostname[128];
 		getlogin_r(username,128);	
 		gethostname(hostname,128);
 		cout << username << "@" << hostname << "$";
-		cin.getline(token, 128);
+
+
+}
+int main(int argc, char *argv[])
+{
+	bool nonstop = true;
+	while(nonstop)
+	{
+		char token[1024] = {0};
+		login();
+		cin.getline(token,128);
 		if(strcmp(token,"exit") == 0)
 		{	
 			exit(0);//if user typed exit, it exits the program.
@@ -40,15 +48,14 @@ int main(int argc, char *argv[])
 		unsigned position = 0;//counts when it gets to end of line using cstring;
 						
 	         
-		char *cmptoken = strtok(token, " ");
+		char *cmptoken = strtok(token, " ;#|");
 		while( cmptoken != NULL)
 		{
 			argument[position] = cmptoken;
 			position = position + 1;
-			cmptoken = strtok(NULL," ");//continues parsing the line
+			cmptoken = strtok(NULL," ;#|");//continues parsing the line
 		}
 		argument[position] = NULL;//ends the strtok with a null to make sure it doesn't seg fault
-			
 		
 		int forkvar = fork();//uses pid to identify processes
 		if(forkvar)//parent process which runs 
@@ -57,11 +64,12 @@ int main(int argc, char *argv[])
 		}	
 		else if(forkvar == 0)//child process which lets us run exec
 		{
-		//	wait(0);
+			wait(0);
 			if(execvp(argument[0], argument) == -1)//takes in the argument from array.
 			{
 				perror("execvp did not run");
 			}
+			delete[] argument;
 			exit(1);//exits when fails
 			
 		}
@@ -70,7 +78,6 @@ int main(int argc, char *argv[])
 			perror("Fork failed");//error flag when forking fails
 			return 0;
 		}
-		delete[] argument;	
 	}
 	return 0;
 
