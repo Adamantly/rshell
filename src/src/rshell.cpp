7203a7db0412b0b6a6f1f48dbb594498de6e073f
Adam Fan <afan004@hammer.cs.ucr.edu>
2014-11-08 15:49:48 -0800

    test

--- src/src/rshell.cpp
+++ src/src/rshell.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<stdlib.h>
 #include<unistd.h>
-#include<string.h>
 #include<cstring>
 #include<stdio.h>
 #include<cstdlib>
@@ -9,73 +8,59 @@
 #include<sys/wait.h>
 using namespace std;
 
-
-void login()
-{
-		char username[128];
-		char hostname[128];
-		if(getlogin_r(username,128) != 0)
-		{
-			perror("Could not get username");
-		}	
-		if(gethostname(hostname,128) != 0)
-		{
-			perror("Could not get hostname");
-		}
-		cout << username << "@" << hostname << "$";
-
-
-}
 int main(int argc, char *argv[])
 {
-	bool nonstop = true;
-	while(nonstop)
+	while(1)
 	{
-		char token[1024] = {0};
-		login();
-		cin.getline(token,128);
+		char token[128];
+		char username[128];
+		char hostname[128];
+		getlogin_r(username,128);	
+		gethostname(hostname,128);
+		cout << username << "@" << hostname << "$";
+		cin.getline(token, 128);
+
 		if(strcmp(token,"exit") == 0)
-		{	
-			exit(0);//if user typed exit, it exits the program.
+		{
+			exit(1);//if user typed exit, it exits the program.
 		}	
-	//	unsigned l = 0; //attempt at comments
+	//	unsigned l = 0;	
 	//	while(token[l] != '\0')
 	//	{
 	//		if(token[l] == '#');
 	//		{
 	//			token[l] = '\0';
-	//		//	cout << "hello" << endl;
 	//		}	
-	//	   ++l;
+	//	    l++;
 	//	}
 		char **argument;
-		argument = new char *[1024];//creates array of pointers
+		argument = new char *[128];//creates array of pointers
 
 		unsigned position = 0;//counts when it gets to end of line using cstring;
 						
 	         
-		char *cmptoken = strtok(token, " ;#|");
+		char *cmptoken = strtok(token, " ");
 		while( cmptoken != NULL)
 		{
 			argument[position] = cmptoken;
-			position = position + 1;
-			cmptoken = strtok(NULL," ;#|");//continues parsing the line
+			for(;position< 128;position++);//cycles through the cstring
+			cmptoken = strtok(NULL," ");//continues parsing the line
 		}
 		argument[position] = NULL;//ends the strtok with a null to make sure it doesn't seg fault
+			
 		
 		int forkvar = fork();//uses pid to identify processes
 		if(forkvar)//parent process which runs 
 		{
 			wait(0);
-		}	
-		else if(forkvar == 0)//child process which lets us run exec
-		{
-			wait(0);
+		}
+			
+		 else if(forkvar == 0)//child process which lets us run exec
+		{	wait(0);
 			if(execvp(argument[0], argument) == -1)//takes in the argument from array.
 			{
 				perror("execvp did not run");
 			}
-			delete[] argument;
 			exit(1);//exits when fails
 			
 		}
@@ -84,6 +69,7 @@ int main(int argc, char *argv[])
 			perror("Fork failed");//error flag when forking fails
 			return 0;
 		}
+		delete []argument;	
 	}
 	return 0;
 
